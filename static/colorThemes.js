6854cc5a2ff6951043848267bdcf32c983882563
Rosia E Evans <willowro@outlook.com>
2024-05-16 18:24:35 +0100

    minor refactor

--- static/colorThemes.js
+++ static/colorThemes.js
@@ -3,9 +3,4 @@ var newSheet = document.getElementById("pageColor").value;
 document.getElementById('colors').setAttribute('href',newSheet);
 }
 
-/*
-var root = document.querySelector(':root');
-var style = root.style
-style.setProperty('--back', '#333333');
-style.setProperty('--text', '#bbbbbb');
-*/
+window.onload = updateColor();
