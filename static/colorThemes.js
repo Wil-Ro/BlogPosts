6e8386cdf489490643bef41e9536a19cbe91b00c
Rosia E Evans <willowro@outlook.com>
2024-05-16 20:30:29 +0100

    colour theme saves in cookies

--- static/colorThemes.js
+++ static/colorThemes.js
@@ -1,6 +1,18 @@
 function updateColor(){
-var newSheet = document.getElementById("pageColor").value;
-document.getElementById('colors').setAttribute('href',newSheet);
+	var newSheet = document.getElementById("pageColor").value;
+	document.getElementById('colors').setAttribute('href',newSheet);
+
+
+	localStorage.setItem("color", newSheet);
 }
 
-window.onload = updateColor();
+function updateColorWithCookieCheck()
+{
+	var cookieColor = localStorage.getItem("color");
+	if (cookieColor != null){
+		document.getElementById("pageColor").value = cookieColor;
+	}
+	updateColor();
+}
+
+window.onload = () => {updateColorWithCookieCheck();};
