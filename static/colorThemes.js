8b7c43cb58cb99153864c7e6853e0ff61e878d36
Rosia E Evans <willowro@outlook.com>
2024-05-16 18:10:24 +0100

    adds changeable colour themes

---
+++ static/colorThemes.js
@@ -0,0 +1,11 @@
+function updateColor(){
+var newSheet = document.getElementById("pageColor").value;
+document.getElementById('colors').setAttribute('href',newSheet);
+}
+
+/*
+var root = document.querySelector(':root');
+var style = root.style
+style.setProperty('--back', '#333333');
+style.setProperty('--text', '#bbbbbb');
+*/
